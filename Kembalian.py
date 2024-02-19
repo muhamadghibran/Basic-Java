7e5c7ec616169f7694e38bb05e08fe9da8bda3a8
muhamadghibran <ripkaka91@gmail.com>
2024-02-19 15:12:39 +0700

    Learning

---
+++ Kembalian.py
@@ -0,0 +1,6 @@
+uangBelanja = float(input(" Masukan Uang belanja :" ))
+totalBelanja = float(input(" Masuka total belanja :"))
+
+uangKembalian = uangBelanja - totalBelanja
+
+print("Uang kembalian : %.0f"%uangKembalian)
